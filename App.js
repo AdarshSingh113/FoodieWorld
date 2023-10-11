ddcaea843ba6e067ee61aebf729055a222ad5436
AshishPrajapati22 <ashishchakrwarti@gmail.com>
2023-10-11 17:22:07 +0530

    Lecture 2

--- App.js
+++ App.js
@@ -1,3 +1,6 @@
+import React from "react"
+import ReactDOM from "react-dom/client"
+
 const parent = React.createElement("div",{id:"parent"},[
 React.createElement("div",{id:"child"},[React.createElement("h1",{},"I m first grandson"),React.createElement("h2",{},"I m first grandaughter")]),
 React.createElement("div",{id:"child2"},[React.createElement("h1",{},"I m first grandson"),React.createElement("h2",{},"I m first grandaughter")])])
---
+++ README.md
@@ -0,0 +1,18 @@
+#Namaste adarsh
+
+# Parcel
+-dev build
+-HMR = Hot Module replacmeent (Refresh)
+-local server
+-File Wattching Alogrithm - written in c++
+-Faster builds due to caching- parcel cache
+-Image Optimization
+-Minification of files
+-Bundling'
+-Compressing
+-Consistent Hashing
+-Code Splittingh
+-Differntial Bundling-support older brousers
+-Https
+-Tree Shaking - Remove the unused code
+-Diff build for dev and prod
