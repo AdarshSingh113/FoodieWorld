585cbdbde263bee30b563c5cb2f5457b1fc38a27
Ashish <shadowak439@gmail.com>
2024-01-16 20:30:52 +0530

    Last epsiodee

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -8,7 +8,6 @@ const Cart =  () =>{
      const handleClearCart = () =>{
         dispatch(clearCart());
      }
-
     const cartItems = useSelector((store)=>store.cart.items);
 
     return <div className="m-10 p-10 text-center" >
@@ -16,6 +15,7 @@ const Cart =  () =>{
         <button className="m-2 p-2 bg-black text-white"
         onClick={handleClearCart}
         >Clear Items</button>
+        
         <div className="w-6/12 m-auto">
             <ItemList items={cartItems}/>
         </div>
